dc4260c99c52209cc517aeb330ee67d2e4565e05
GustiAmelia <gusti_amelia@apps.ipb.ac.id>
2020-08-17 21:25:26 +0700

    update register

--- src/Models/auth.js
+++ src/Models/auth.js
@@ -6,37 +6,47 @@ const jwt = require("jsonwebtoken");
 const authModel ={
   postNewUser : (body)=>{
     return new Promise((resolve,reject)=>{
-
-      bcrypt.genSalt(10,(error,salt)=>{
-        if(error){
-          reject(error);
+      const querySelect='SELECT username FROM users WHERE username=?';
+      connection.query(querySelect,[body.username],(error,results)=>{
+        if(!results){
+          reject(error)
         }
-
-        const{password}=body;
-        bcrypt.hash(password,salt,(error,hashedPassword)=>{
-          if(error){
-            reject(error);
-          }
-          const newBody={...body, password:hashedPassword};
-          const qs = 'INSERT INTO users SET ?';
-          connection.query(qs,newBody,(error,results)=>{
-            if(!error){
-                resolve(results);
-            }else{
-                reject(error);
+        if(results.length){
+          reject({
+            msg:'username is already taken!'
+          })
+        }else{
+          bcrypt.genSalt(10,(error,salt)=>{
+            if(error){
+              reject(error);
             }
+    
+            const{password}=body;
+            bcrypt.hash(password,salt,(error,hashedPassword)=>{
+              if(error){
+                reject(error);
+              }
+              const newBody={...body, password:hashedPassword};
+              const qs = 'INSERT INTO users SET ?';
+              connection.query(qs,newBody,(error,results)=>{
+                console.log(newBody);
+                if(!error){
+                    resolve(results);
+                }else{
+                    reject(error);
+                }
+              });
+            });
           });
-        });
-      });
+        }
+      })
+      
     });
   },
   loginUser :(body)=>{
     return new Promise((resolve,reject)=>{
       const qs = 'SELECT username, password, level_id FROM users WHERE username=?';
       connection.query(qs, body.username ,(error,results)=>{
-        if(error){
-          reject(error);
-        }
         if(!results.length){
           reject('User Not Found');
         }
@@ -65,8 +75,7 @@ const authModel ={
         }
       });
     });
-
-  }
+  },
 }
 
 module.exports=authModel;
